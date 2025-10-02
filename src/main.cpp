b9624ba368f693cefa278cb4fb81190d02137b4a
bunter12 <home52690@gmail.com>
2025-10-02 20:53:11 +0300

    refactor main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,117 +1,164 @@
+#include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-#include <iostream>
-#include <string>
-#include <sstream>
-#include <fstream>
-#include <unistd.h>
-#include "math/math.h"
 
-std::string loadShadersSource(const std::string& filePath){
-    std::ifstream file(filePath);
-    if(!file.is_open()){
-        std::cerr<<"Can not open file: "<<filePath<<std::endl;
-        return "";
-    }
-    std::stringstream buffer;
-    buffer<<file.rdbuf();
-    return buffer.str();
+#define STB_IMAGE_IMPLEMENTATION
+#include "../vendor/stb/stb_image.h"
+
+#include "math/math.h"
+#include "shaders.h"
+
+const unsigned int SCREEN_WIDTH = 800;
+const unsigned int SCREEN_HEIGHT = 600;
+
+#define GL_CALL(x) do { \
+    clearGlErrors(); \
+    x; \
+    printGlErrors(#x, __FILE__, __LINE__); \
+} while (0)
+
+static void clearGlErrors() {
+    while (glGetError() != GL_NO_ERROR);
 }
 
+static bool printGlErrors(const char* function, const char* file, int line) {
+    bool hasError = false;
+    GLenum error;
+    while ((error = glGetError()) != GL_NO_ERROR) {
+        std::string errorString;
+        switch (error) {
+            case GL_INVALID_ENUM: errorString = "GL_INVALID_ENUM"; break;
+            case GL_INVALID_VALUE: errorString = "GL_INVALID_VALUE"; break;
+            case GL_INVALID_OPERATION: errorString = "GL_INVALID_OPERATION"; break;
+            case GL_OUT_OF_MEMORY: errorString = "GL_OUT_OF_MEMORY"; break;
+            default: errorString = "Unknown Error"; break;
+        }
+        std::cerr << "[OpenGL Error] (" << errorString << "): in function '" << function << "' at " << file << ":" << line << std::endl;
+        hasError = true;
+    }
+    return hasError;
+}
+
+
 int main() {
-    Vec2 pos(100, 200);
-        Vec2 speed(5, 10);
-        Vec2 new_pos = pos + speed;
-        std::cout << "Vec2 test: (" << new_pos.x << ", " << new_pos.y << ")" << std::endl;
-    
-    Mat4 translation = Mat4::translate(Vec3(50, -30, 0));
-        std::cout << "Mat4 translation test (x, y, z): "
-                  << translation.m[12] << ", "
-                  << translation.m[13] << ", "
-                  << translation.m[14] << std::endl;
-    
-    if(!glfwInit())
+    if (!glfwInit()) {
+        std::cerr << "Failed to initialize GLFW" << std::endl;
         return -1;
-    
+    }
+
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 #ifdef __APPLE__
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
-        
-    GLFWwindow* Window = glfwCreateWindow(800,600,"MyEngine",NULL,NULL);
-    
-    if(!Window){
+
+    GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "MyEngine", NULL, NULL);
+    if (!window) {
+        std::cerr << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
         return -1;
     }
-    
-    glfwMakeContextCurrent(Window);
-    
+    glfwMakeContextCurrent(window);
+
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cerr << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
-    glViewport(0,0,800,600);
     
-    std::string vertexShaderSource = loadShadersSource("src/shaders/basic.vert");
-    std::string fragmentShaderSource = loadShadersSource("src/shaders/basic.frag");
-    const char * vsSource = vertexShaderSource.c_str();
-    const char * fsSource = fragmentShaderSource.c_str();
+    glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+    glEnable(GL_BLEND);
+
+    Shader ourShader("src/shaders/basic.vert", "src/shaders/basic.frag");
     
-    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vsSource,NULL);
-    glCompileShader(vertexShader);
-    
-    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fsSource,NULL);
-    glCompileShader(fragmentShader);
-    
-    unsigned int shaderProgramm = glCreateProgram();
-    glAttachShader(shaderProgramm, vertexShader);
-    glAttachShader(shaderProgramm, fragmentShader);
-    glLinkProgram(shaderProgramm);
-    
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
-    
+    ourShader.use();
+    ourShader.setInt("ourTexture", 0);
+
     float vertices[] = {
-        -0.5f, -0.5f, 0.0f,   1.0f, 0.0f, 0.0f,
-         0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,
-         0.0f,  0.5f, 0.0f,   0.0f, 0.0f, 1.0f
+
+         50.0f,  50.0f, 0.0f,       1.0f, 1.0f,
+         50.0f, -50.0f, 0.0f,       1.0f, 0.0f,
+        -50.0f, -50.0f, 0.0f,       0.0f, 0.0f,
+        -50.0f,  50.0f, 0.0f,       0.0f, 1.0f
     };
-    
-    unsigned int VBO, VAO;
+
+    unsigned int indices[] = {
+        0, 1, 3,
+        1, 2, 3
+    };
+
+    unsigned int VBO, VAO, EBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
-    
+    glGenBuffers(1, &EBO);
+
     glBindVertexArray(VAO);
-    
+
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
     
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *) 0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
     
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)(3 * sizeof(float)));
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
-    
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
     glBindVertexArray(0);
+
+    unsigned int texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
     
-    while(!glfwWindowShouldClose(Window)){
-        
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    int width, height, nrChannels;
+    stbi_set_flip_vertically_on_load(true);
+    unsigned char* data = stbi_load("assets/player.png", &width, &height, &nrChannels, 0);
+    
+    if (data) {
+        GLenum format = (nrChannels == 4) ? GL_RGBA : GL_RGB;
+        glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    } else {
+        std::cerr << "Failed to load texture. Reason: " << stbi_failure_reason() << std::endl;
+    }
+    stbi_image_free(data);
+
+    Mat4 projection = Mat4::ortho(0.0f, (float)SCREEN_WIDTH, 0.0f, (float)SCREEN_HEIGHT, -1.0f, 1.0f);
+
+    while (!glfwWindowShouldClose(window)) {
+ 
         glClearColor(0.1f, 0.2f, 0.4f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        ourShader.use();
         
-        glUseProgram(shaderProgramm);
+        Mat4 model = Mat4::translate(Vec3(200.0f, 100.0f, 0.0f));
+        ourShader.setMat4("projection", projection);
+        ourShader.setMat4("model", model);
+        
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture);
+
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-        
-        glfwSwapBuffers(Window);
+        GL_CALL(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0));
+
+        glfwSwapBuffers(window);
         glfwPollEvents();
     }
     
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
+    glDeleteProgram(ourShader.ID);
+
     glfwTerminate();
     return 0;
 }
